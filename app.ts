8cc7ee1bf3f0e386657afea2f4511a8605e4e1f4
Andrew Peace <apeace@gmail.com>
2015-11-18 13:14:59 -0500

    Show equation in box by default

--- app.ts
+++ app.ts
@@ -4,8 +4,9 @@ import str = require('./lib/str');
 
 let input = <HTMLInputElement>(document.getElementById('equation'));
 let output = <HTMLElement>(document.getElementById('output'));
+input.addEventListener('keyup', parseInput);
 
-input.addEventListener('keyup', () => {
+function parseInput () {
   let inputStr = input.value;
   let parsed: e.Equation;
   let html: string;
@@ -17,4 +18,7 @@ input.addEventListener('keyup', () => {
     return;
   }
   output.innerHTML = html;
-});
+}
+
+input.value = '4x^3 + 2x^2 + 3x + 10';
+parseInput();
